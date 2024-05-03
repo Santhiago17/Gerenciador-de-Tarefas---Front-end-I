945692df15651da0b8d831192facee9c05a61036
Santhiago <santhiagosantos7@gmail.com>
2024-05-02 22:48:25 -0300

    Finalizada a estilização e correção da lógica JS

--- script.js
+++ script.js
@@ -2,6 +2,11 @@
 function addTarefa() {
   let titulo = document.getElementById("txtTitulo").value;
   let descricao = document.getElementById("txtTarefa").value;
+
+  if(titulo === "" || descricao === ""){
+    alert('Por favor, preencha todos os campos antes de clicar em adicionar!')
+    return;
+  }
   
   let novaLinha = document.createElement("tr");
   
@@ -15,6 +20,7 @@ function addTarefa() {
   botaoExcluir.innerText = 'Excluir';
   botaoExcluir.setAttribute('type','button');
   botaoExcluir.setAttribute('onclick','excluirTarefa(this)');
+  botaoExcluir.classList.add('btnExcluir');
 
   let celulaAcao = document.createElement('td');
   celulaAcao.appendChild(botaoExcluir);
@@ -23,7 +29,10 @@ function addTarefa() {
   novaLinha.appendChild(novaDescricao);
   novaLinha.appendChild(celulaAcao);
 
-  document.getElementById('tabela').appendChild(novaLinha);
+  document.getElementById('tabela').getElementsByTagName('tbody')[0].appendChild(novaLinha);
+
+  document.getElementById('txtTitulo').value = "";
+  document.getElementById('txtTarefa').value = "";
 }
 
 function excluirTarefa(btn){
